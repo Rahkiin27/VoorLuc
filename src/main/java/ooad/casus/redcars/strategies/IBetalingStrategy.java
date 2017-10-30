c82e0117f94ac69d326f58e604a590afdc001e89
Dustin <DusKwakernaat@gmail.com>
2017-10-30 14:25:25 +0100

    Eerste commit

--- src/main/java/ooad/casus/redcars/strategies/IBetalingStrategy.java
+++ src/main/java/ooad/casus/redcars/strategies/IBetalingStrategy.java
@@ -1,8 +1,5 @@
 package ooad.casus.redcars.strategies;
 
-/**
- * Created by Dustin on 10/29/2017.
- */
 public interface IBetalingStrategy {
     double getPrijsPerKilometer();
     double getPrijsPerWeek();
