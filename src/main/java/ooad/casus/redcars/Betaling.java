85cba3dae07fdd51291c49e478b1d93f1fc77196
Dustin <DusKwakernaat@gmail.com>
2017-10-30 15:11:43 +0100

    Eerste commit

--- src/main/java/ooad/casus/redcars/Betaling.java
+++ src/main/java/ooad/casus/redcars/Betaling.java
@@ -8,8 +8,10 @@ import ooad.casus.redcars.strategies.*;
 public class Betaling {
     private IBetalingStrategy betalingStrategy;
     private BetalingBerekeningen berekening;
+    private PeriodeType periodeType;
 
     public Betaling(AbonnementType abonnementType, PeriodeType periodeType, AutoType autoType, double kilometers) {
+        this.periodeType = periodeType;
         switch(abonnementType) {
             case BETAALD:
                 if (autoType == AutoType.PERSONEN) {
@@ -28,10 +30,47 @@ public class Betaling {
                 }
                 break;
         }
-        berekening = new BetalingBerekeningen(betalingStrategy, periodeType, kilometers);
+        berekening = new BetalingBerekeningen(betalingStrategy, kilometers);
     }
 
     public double berekenTotaalprijs(int periodeDuur, int urenGereden) {
-        return berekening.berekenTotaalprijs(periodeDuur, urenGereden);
+        int periodeOverschredenUren = 0;
+        switch (periodeType) {
+            case UUR:
+                if (urenGereden > periodeDuur) {
+                    periodeOverschredenUren = urenGereden - periodeDuur;
+                }
+                break;
+            case DAG:
+                if (urenGereden > periodeDuur * 24) {
+                    periodeOverschredenUren = urenGereden - periodeDuur * 24;
+                }
+                break;
+            case WEEKEND:
+                if (urenGereden > periodeDuur * 48) {
+                    periodeOverschredenUren = urenGereden - periodeDuur * 48;
+                }
+                break;
+            case WEEK:
+                if (urenGereden > periodeDuur * 168) {
+                    periodeOverschredenUren = urenGereden - periodeDuur * 168;
+                }
+                break;
+        }
+        return berekening.berekenTotaalprijs(berekenPeriodePrijs(periodeType, periodeDuur), berekenPeriodePrijs(PeriodeType.UUR, periodeOverschredenUren));
+    }
+
+    private double berekenPeriodePrijs(PeriodeType periodeType, int periode) {
+        switch (periodeType) {
+            case UUR:
+                return betalingStrategy.getPrijsPerUur() * periode;
+            case DAG:
+                return betalingStrategy.getPrijsPerDag() * periode;
+            case WEEKEND:
+                return betalingStrategy.getPrijsPerWeekend() * periode;
+            case WEEK:
+                return betalingStrategy.getPrijsPerWeek() * periode;
+        }
+        return 0;
     }
 }
--- src/main/java/ooad/casus/redcars/BetalingBerekeningen.java
+++ src/main/java/ooad/casus/redcars/BetalingBerekeningen.java
@@ -5,70 +5,26 @@ import ooad.casus.redcars.strategies.IBetalingStrategy;
 
 public class BetalingBerekeningen {
     private IBetalingStrategy betalingStrategy;
-    private PeriodeType periodeType;
     private double kilometers;
 
-    public BetalingBerekeningen(IBetalingStrategy betalingStrategy, PeriodeType periodeType, double kilometers) {
+    public BetalingBerekeningen(IBetalingStrategy betalingStrategy, double kilometers) {
         this.betalingStrategy = betalingStrategy;
-        this.periodeType = periodeType;
         this.kilometers = kilometers;
     }
 
-    public double berekenTotaalprijs(int periodeDuur, int urenGereden) {
-        int periodeOverschredenUren = 0;
-        switch(periodeType) {
-            case UUR:
-                if (urenGereden > periodeDuur) {
-                    periodeOverschredenUren = urenGereden - periodeDuur;
-                }
-                break;
-            case DAG:
-                if (urenGereden > periodeDuur * 24) {
-                    periodeOverschredenUren = urenGereden - periodeDuur * 24;
-                }
-                break;
-            case WEEKEND:
-                if (urenGereden > periodeDuur * 48) {
-                    periodeOverschredenUren = urenGereden - periodeDuur * 48;
-                }
-                break;
-            case WEEK:
-                if (urenGereden > periodeDuur * 168) {
-                    periodeOverschredenUren = urenGereden - periodeDuur * 168;
-                }
-                break;
-        }
-        System.out.println("km: " + berekenKilometerprijs() + " | huur: " + berekenHuurprijs(periodeDuur) + " | boete:" + berekenBoetebedrag(periodeDuur, periodeOverschredenUren));
-        return berekenKilometerprijs() + berekenHuurprijs(periodeDuur) + berekenBoetebedrag(periodeDuur, periodeOverschredenUren);
+    public double berekenTotaalprijs(double periodebedrag, double boetebedrag) {
+        boetebedrag = berekenBoetebedrag(boetebedrag, periodebedrag);
+        System.out.println("km: " + berekenKilometerprijs() + " | huur: " + periodebedrag + " | boete:" + boetebedrag);
+        return berekenKilometerprijs() + periodebedrag + boetebedrag;
     }
 
     private double berekenKilometerprijs() {
         return betalingStrategy.getPrijsPerKilometer() * kilometers;
     }
 
-    private double berekenHuurprijs(int periodeDuur) {
-        return berekenPeriodePrijs(periodeType, periodeDuur);
-    }
-
-    private double berekenBoetebedrag(int periodeDuur, int periodeOverschredenUren) {
-        if (periodeOverschredenUren > 0) {
-            double huurPrijs = berekenPeriodePrijs(periodeType, periodeDuur);
-            double boetebedrag = berekenPeriodePrijs(PeriodeType.UUR, periodeOverschredenUren);
-            return huurPrijs + boetebedrag;
-        }
-        return 0;
-    }
-
-    private double berekenPeriodePrijs(PeriodeType periodeType, int periode) {
-        switch(periodeType) {
-            case UUR:
-                return betalingStrategy.getPrijsPerUur() * periode;
-            case DAG:
-                return betalingStrategy.getPrijsPerDag() * periode;
-            case WEEKEND:
-                return betalingStrategy.getPrijsPerWeekend() * periode;
-            case WEEK:
-                return betalingStrategy.getPrijsPerWeek() * periode;
+    private double berekenBoetebedrag(double boetebedrag, double periodebedrag) {
+        if (boetebedrag > 0) {
+            return boetebedrag + periodebedrag;
         }
         return 0;
     }
