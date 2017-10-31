788014e4d355bf1a7e9d013d4a4bb52abad08850
LLinders <l.linders@student.han.nl>
2017-10-31 19:52:42 +0100

    gebruiker kan actief of inactief gezet worden

--- src/main/java/ooad/casus/redcars/Gebruiker.java
+++ src/main/java/ooad/casus/redcars/Gebruiker.java
@@ -10,6 +10,7 @@ public class Gebruiker {
     private String voornaam, achternaam, email, adres, woonplaats, bankrekeningnummer;
     private AbonnementType abonnementType;
     private Lidmaatschapspas lidmaatschapspas;
+    private boolean isActief;
     private Reservering reservering;
 
     public Gebruiker(String voornaam, String achternaam, String email, String adres, String woonplaats, String bankrekeningnummer, AbonnementType abonnementType) {
@@ -21,18 +22,53 @@ public class Gebruiker {
         this.abonnementType = abonnementType;
         this.bankrekeningnummer = bankrekeningnummer;
 
+        isActief = true;
+
         lidmaatschapspas = new Lidmaatschapspas();
     }
 
     public void maakReservering(PeriodeType periodeType, AutoType autoType, LocalDateTime beginTijd) {
-        reservering = new Reservering(periodeType, autoType, abonnementType, beginTijd);
+        if (isActief) {
+            reservering = new Reservering(periodeType, autoType, abonnementType, beginTijd);
+        }
     }
 
     public double berekenKosten() {
-        return reservering.berekenKosten();
+        if (reservering != null) {
+            return reservering.berekenKosten();
+        }
+        return 0;
     }
 
     public void setAbonnementType(AbonnementType abonnementType) {
         this.abonnementType = abonnementType;
+    }
+
+    public void setActief(boolean actief) {
+        isActief = actief;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public void setBankrekeningnummer(String bankrekeningnummer) {
+        this.bankrekeningnummer = bankrekeningnummer;
     }
 }
